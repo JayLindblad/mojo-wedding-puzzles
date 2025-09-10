d9dce4919c993d382cd039a77ed712905eccea81
JayLindblad <58444123+JayLindblad@users.noreply.github.com>
2025-09-10 11:27:44 -0400

    Update db.js
    Disabled firebase and output to console

--- base/scripts/db.js
+++ base/scripts/db.js
@@ -1,14 +1,19 @@
+// db.js - disabled because Firebase is not being used
+console.log("Database functionality disabled - no user tracking enabled")
+
+
+
 // Initialize Firestore through Firebase
-const app = firebase.initializeApp(keys);
-var analytics = firebase.analytics(app);
+// const app = firebase.initializeApp(keys);
+// var analytics = firebase.analytics(app);
 
-var userId = "";
-if (localStorage.getItem("userId") == null) {
-	userId = crypto.randomUUID();
-	localStorage.setItem("userId", userId);
-} else {
-	userId = localStorage.getItem("userId");
-};
-analytics.setUserId(userId);
+// var userId = "";
+// if (localStorage.getItem("userId") == null) {
+//	userId = crypto.randomUUID();
+//	localStorage.setItem("userId", userId);
+// } else {
+// 	userId = localStorage.getItem("userId");
+//};
+// analytics.setUserId(userId);
 
-var db = firebase.firestore();
+// var db = firebase.firestore();
